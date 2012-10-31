b09f74f4266fe1e16ca0be976f87d4653b7fcbd1
cxjava <cxjava@gmail.com>
2012-10-31 17:36:58 +0800

    beginning！！！

---
+++ j2ee-parent/j2ee-extjs/src/main/java/com/maty/j2ee/common/jackjson/Jackson.java
@@ -0,0 +1,167 @@
+package com.maty.j2ee.common.jackjson;
+
+import java.io.IOException;
+import java.util.Set;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import com.fasterxml.jackson.core.JsonGenerationException;
+import com.fasterxml.jackson.core.JsonParseException;
+import com.fasterxml.jackson.core.type.TypeReference;
+import com.fasterxml.jackson.databind.JsonMappingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.ser.FilterProvider;
+import com.fasterxml.jackson.databind.ser.impl.SimpleBeanPropertyFilter;
+import com.fasterxml.jackson.databind.ser.impl.SimpleFilterProvider;
+
+/**
+ * jackjson一些转换方法
+ * 
+ * @author chenxin
+ * @date 2010-6-28 下午04:07:33
+ */
+public class Jackson {
+	private static final Logger logger = LoggerFactory.getLogger(Jackson.class);
+	/** 格式化时间的string */
+	//private static final String DATE_TIME_FORMAT = "yyyy-MM-dd HH:mm:ss";
+
+	/**
+	 * fromJsonToObject<br>
+	 * jackjson把json字符串转换为Java对象的实现方法
+	 * 
+	 * <pre>
+	 * return Jackson.jsonToObj(this.answersJson, new TypeReference&lt;List&lt;StanzaAnswer&gt;&gt;() {
+	 * });
+	 * </pre>
+	 * 
+	 * @param <T>
+	 *            转换为的java对象
+	 * @param json
+	 *            json字符串
+	 * @param typeReference
+	 *            jackjson自定义的类型
+	 * @return 返回Java对象
+	 */
+	public static <T> T jsonToObj(String json, TypeReference<T> typeReference) {
+		ObjectMapper mapper = new ObjectMapper();
+		try {
+			return mapper.readValue(json, typeReference);
+		} catch (JsonParseException e) {
+			logger.error("JsonParseException: ", e);
+		} catch (JsonMappingException e) {
+			logger.error("JsonMappingException: ", e);
+		} catch (IOException e) {
+			logger.error("IOException: ", e);
+		}
+		return null;
+	}
+
+	/**
+	 * fromJsonToObject<br>
+	 * json转换为java对象
+	 * 
+	 * <pre>
+	 * return Jackson.jsonToObj(this.answersJson, Jackson.class);
+	 * </pre>
+	 * 
+	 * @param <T>
+	 *            要转换的对象
+	 * @param json
+	 *            字符串
+	 * @param valueType
+	 *            对象的class
+	 * @return 返回对象
+	 */
+	public static <T> T jsonToObj(String json, Class<T> valueType) {
+		ObjectMapper mapper = new ObjectMapper();
+		try {
+			return mapper.readValue(json, valueType);
+		} catch (JsonParseException e) {
+			logger.error("JsonParseException: ", e);
+		} catch (JsonMappingException e) {
+			logger.error("JsonMappingException: ", e);
+		} catch (IOException e) {
+			logger.error("IOException: ", e);
+		}
+		return null;
+	}
+
+	/**
+	 * fromObjectToJson<br>
+	 * java对象转换为json字符串
+	 * 
+	 * @param object
+	 *            Java对象
+	 * @return 返回字符串
+	 */
+	public static String objToJson(Object object) {
+		ObjectMapper mapper = new ObjectMapper();
+		try {
+			return mapper.writeValueAsString(object);
+		} catch (JsonGenerationException e) {
+			logger.error("JsonGenerationException: ", e);
+		} catch (JsonMappingException e) {
+			logger.error("JsonMappingException: ", e);
+		} catch (IOException e) {
+			logger.error("IOException: ", e);
+		}
+		return null;
+	}
+
+	/**
+	 * fromObjectToJson<br>
+	 * java对象转换为json字符串
+	 * 
+	 * @param object
+	 *            要转换的对象
+	 * @param filterName
+	 *            过滤器的名称
+	 * @param properties
+	 *            要过滤哪些字段
+	 * @return
+	 */
+	public static String objToJson(Object object, String filterName, Set<String> properties) {
+		ObjectMapper mapper = new ObjectMapper();
+		FilterProvider filters = new SimpleFilterProvider().addFilter(filterName, SimpleBeanPropertyFilter.serializeAllExcept(properties));
+		try {
+			mapper.setFilters(filters);
+			return mapper.writeValueAsString(object);
+		} catch (JsonGenerationException e) {
+			logger.error("JsonGenerationException: ", e);
+		} catch (JsonMappingException e) {
+			logger.error("JsonMappingException: ", e);
+		} catch (IOException e) {
+			logger.error("IOException: ", e);
+		}
+		return null;
+	}
+
+	/**
+	 * fromObjectToJson<br>
+	 * java对象转换为json字符串
+	 * 
+	 * @param object
+	 *            要转换的对象
+	 * @param filterName
+	 *            过滤器的名称
+	 * @param properties
+	 *            要过滤的字段名称
+	 * @return
+	 */
+	public static String objToJson(Object object, String filterName, String property) {
+		ObjectMapper mapper = new ObjectMapper();
+		FilterProvider filters = new SimpleFilterProvider().addFilter(filterName, SimpleBeanPropertyFilter.serializeAllExcept(property));
+		try {
+			mapper.setFilters(filters);
+			return mapper.writeValueAsString(object);
+		} catch (JsonGenerationException e) {
+			logger.error("JsonGenerationException: ", e);
+		} catch (JsonMappingException e) {
+			logger.error("JsonMappingException: ", e);
+		} catch (IOException e) {
+			logger.error("IOException: ", e);
+		}
+		return null;
+	}
+}
---
+++ j2ee-parent/j2ee-extjs/src/main/webapp/WEB-INF/views/login.jsp
@@ -0,0 +1,46 @@
+<%@ page language="java" contentType="text/html; charset=UTF-8"%>
+<%@ taglib prefix="c" uri="http://java.sun.com/jsp/jstl/core" %>
+<%@ taglib prefix="spring" uri="http://www.springframework.org/tags" %>
+<c:set var="ctx" value="${pageContext.request.contextPath}"/>
+<c:set var="lang" value="${sessionScope['org.springframework.web.servlet.i18n.SessionLocaleResolver.LOCALE']}"/>
+<c:set var="language" value="${lang==null?'en_US':lang}"/>
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head>
+	<meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />
+	<title><spring:message code="login.page.title"/></title>
+	<link rel="shortcut icon" href="${ctx }/resources/images/favicon.ico" type="image/x-icon">
+	<link rel="icon" href="${ctx }/resources/images/favicon.ico" type="image/x-icon">
+	<script src="${ctx}/resources/libs/yepnope/yepnope.1.5.4.min.js" type="text/javascript"></script>
+	<script type="text/javascript">
+		ctx = "${ctx}";
+		language="${language}";
+		yepnope({
+			load : [ //extjs
+			         "${ctx}/resources/libs/extjs/resources/css/ext-all.css",
+					 "${ctx}/resources/libs/extjs/ext-all.js", 
+					 "${ctx}/resources/libs/extjs/locale/ext-lang-${language}.js", 
+					 //jquery
+					 "${ctx}/resources/libs/jquery/jquery-1.7.2.min.js", 
+					 "${ctx}/resources/libs/jquery/jquery-plugin/jquery.json-2.3-min.js", 
+					 "${ctx}/resources/libs/jquery/jquery-plugin/jquery.center-min.js",
+			         "${ctx}/resources/css/default.css"
+					 ],
+			complete : function() {
+				/* Ext.QuickTips.init();
+				Ext.form.Field.prototype.msgTarget = 'title';//qtip,title,under,side
+				Ext.state.Manager.setProvider(new Ext.state.CookieProvider());
+				Ext.BLANK_IMAGE_URL = '${ctx}/resources/extjs/resources/images/default/s.gif';
+				yepnope("${ctx}/resources/js/login.js");
+				$("#logo-table a").html('<img src="${ctx}/resources/images/login.gif"></img>');*/
+				} 
+			});
+	</script>
+</head>
+
+<body>
+	<a href="?locale=zh_CN">中文</a>
+    <a href="?locale=en_US">English</a>
+    <a href="?locale=de_DE">Deutsch</a>
+</body>
+</html>
