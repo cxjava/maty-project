6663e0720bdf97136397810dfb45bf63cf826b2b
cxjava <cxjava@gmail.com>
2012-11-05 17:55:46 +0800

    add more test method

--- j2ee-parent/j2ee-extjs/src/main/java/com/maty/j2ee/repository/UserRepository.java
+++ j2ee-parent/j2ee-extjs/src/main/java/com/maty/j2ee/repository/UserRepository.java
@@ -2,11 +2,17 @@ package com.maty.j2ee.repository;
 
 import java.util.List;
 
-import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.repository.PagingAndSortingRepository;
 
 import com.maty.j2ee.entity.User;
 
-public interface UserRepository extends CrudRepository<User, Integer> {
+public interface UserRepository extends PagingAndSortingRepository<User, Integer> {
 
 	public List<User> findByAccount(String account);
+
+	List<User> findByAccountAndName(String account, String name);
+	List<User> findByAccountIgnoreCaseAndName(String account, String name);
+
+	List<User> findByAccountAndNameOrPassword(String account, String name, String password);
+
 }
